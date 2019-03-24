f51e87d1f22e2df6e0656d1772b2be37e298052a
Mondit Thumniramon <mondit.thum@gmail.com>
2019-03-24 23:58:24 +0700

    Add Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,18 @@
 import React from 'react'
 
+import iconFB from '../images/icon_fb.png'
+import iconTwitter from '../images/icon_twitter.png'
+
 const Footer = () => {
   return (
-    <div>
-      Footer
+    <div className="footer">
+      <div className="link-list">
+        <a href="#" target="_blank"><img src={iconFB} alt="" /></a>
+        <a href="#" target="_blank"><img src={iconTwitter} alt="" /></a>
+        <a href="#" target="_blank"><p>How To</p></a>
+        <a href="#" target="_blank"><p>Credit</p></a>
+        <a href="#" target="_blank"><p>Contact</p></a>
+      </div>
     </div>
   )
 }
