f43b76883b8fb0d231fe9aef775e11d25cdfa75f
Mondit Thumniramon <mondit.thum@gmail.com>
2019-04-03 01:40:13 +0700

    Handling Header UI

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,18 @@
 import React from 'react'
+import _ from 'lodash'
 
 import AppLogo from './AppLogo'
 import Category from './Category'
 
 import iconMe from '../images/me.png'
 
-const Header = () => {
+const Header = ({ isLoggedIn, location }) => {
+  console.log({ isLoggedIn, location })
+  const showCategory = () => {
+    const currentPage = _.get(location, 'pathname', '')
+    return currentPage !== '/login' && currentPage !== '/add'
+  }
+
   return (
     <div className="header fixed-top">
       <div className="container">
@@ -14,10 +21,10 @@ const Header = () => {
             <AppLogo />
             <p>Confess your Regret and sail it away!</p>
           </div>
-          <div className="category-list">
+          <div className={`category-list ${showCategory() ? 'show' : ''}`}>
             <Category />
           </div>
-          <div className="user-zone">
+          <div className={`user-zone ${isLoggedIn} ? 'show' : ''`}>
             <img src={iconMe} alt="" />
           </div>
         </div>
