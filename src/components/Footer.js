ecfafa7846b953b67a8a325f5dd24a10d279bcdf
Mondit Thumniramon <mondit.thum@gmail.com>
2019-04-28 16:49:15 +0700

    Add Share buttons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,17 +1,39 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import {
+  FacebookShareButton,
+  FacebookIcon,
+  TwitterIcon,
+  TwitterShareButton,
+} from 'react-share';
 
-import iconFB from '../images/icon_fb.png'
-import iconTwitter from '../images/icon_twitter.png'
+// import iconFB from '../images/icon_fb.png'
+// import iconTwitter from '../images/icon_twitter.png'
 import iconLogout from '../images/logout.png'
+
+const url = "https://thatship.netlify.com"
+const title = 'that ship has sailed.'
 
 const Footer = () => {
   return (
     <div className="footer">
       <div className="link-list">
-        {/* <a href="#" target="_blank"><img src={iconFB} alt="" /></a> */}
-        <a href="#" target="_blank"><img src={iconFB} alt="" /></a>
-        <a href="#" target="_blank"><img src={iconTwitter} alt="" /></a>
+        <a href="#">
+          <FacebookShareButton
+            url={url}
+            quote={title}
+            className="Demo__some-network__share-button">
+            <FacebookIcon size={20} round={true} />
+          </FacebookShareButton>
+        </a>
+        <a href="#">
+          <TwitterShareButton
+            url={url}
+            title={title}
+            className="Demo__some-network__share-button">
+            <TwitterIcon size={20} round={true} />
+          </TwitterShareButton>
+        </a>
         <Link to="/about"><p>About</p></Link>
         <Link to="/credit"><p>Credit</p></Link>
         <Link to="/terms"><p>Term & Condition</p></Link>
